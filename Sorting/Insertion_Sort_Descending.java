cf77cd144bb3737e2b4aace342d2ff195a09b1ce
Snehasish-Malik <snehasishmalik@gmail.com>
2022-01-12 18:28:18 +0530

    Insertion Descending added

---
+++ Sorting/Insertion_Sort_Descending.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Insertion_Sort_Descending
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n,key=0,j=0;
+    System.out.print("Enter the size: ");
+    n=sc.nextInt();
+    int ar[]=new int[n];
+    System.out.println("Enter the elements:");
+    for(int i=0;i<n;i++)
+      ar[i]=sc.nextInt();
+
+    
+
+    System.out.println("The sorted aray is : ");
+    for(int i=0;i<n;i++)
+      System.out.println(ar[i]);
+  }
+}
