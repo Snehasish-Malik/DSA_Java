9063888df451b9884d62577d310d3fbd69a1fb35
Snehasish-Malik <snehasishmalik@gmail.com>
2022-01-12 18:27:35 +0530

    Insertion Added

---
+++ Sorting/Insertion_Sort_Ascending.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Insertion_Sort_Ascending
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n,key=0,j=0;
+    System.out.print("Enter the size: ");
+    n=sc.nextInt();
+    int ar[]=new int[n];
+    System.out.println("Enter the elements:");
+    for(int i=0;i<n;i++)
+      ar[i]=sc.nextInt();
+    for(int i=1;i<n;i++)
+    {
+      key=ar[i];
+      j=i-1;
+      while(ar[j]>ar[j+1] && j>=0)
+      {
+        ar[j+1]=ar[j];
+        j--;
+      }
+      ar[j+1]=key;
+    }
+    System.out.println("The sorted aray is : ");
+    for(int i=0;i<n;i++)
+      System.out.println(ar[i]);
+  }
+}
